afce4b600dab0daba71c6d4ed90630a0f0d47968
Dmytro Slobodian <dmytro.slobodian@wundermanthompson.com>
2022-03-16 12:21:10 +0100

    component: new component Search Block

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-import PostBlock from './components/Postblock.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import PostBlock from "./components/Postblock.vue";
+import SearchBlock from "./components/SearchBlock.vue";
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Awesome project" />
-  <PostBlock/>
+  <PostBlock />
+  <SearchBlock />
 </template>
 
 <style>
@@ -21,27 +23,27 @@ import PostBlock from './components/Postblock.vue'
   margin-top: 60px;
 }
 
-:root{
+:root {
   --main-bg-color: #f1f1f1;
   --main-bgdiv-color: #ffffff;
   --text-color-darkness: #333333;
   --text-color-lightness: #bebebe;
   --text-color-light: #777777;
   --selected-div-color: #e94e4e;
-  --arrow-div-color:#3b3b3b;
-  --size-blog-name-text:2.2em;
-  --size-blog-info-text:0.9em;
-  --size-blog-main-text:1em;
-  --font-family-text:'Inconsolata', monospace;
-  --spacing-blog-name-text:0.3em;
-  --spacing-blog-info-text:0.2em;
-  --spacing-blog-main-text:0.1em;
-  --size-right-block-name-text:1.4em;
-  --size-right-block-info-text:0.5em;
-  --size-right-block-main-text:0.8em;
-  --font-right-block-main-text:'Inconsolata', monospace;
-  --spacing-right-block-name-text:0.1em;
-  --spacing-right-block-info-text:0.1em;
-  --spacing-right-block-main-text:0.1em;
+  --arrow-div-color: #3b3b3b;
+  --size-blog-name-text: 2.2em;
+  --size-blog-info-text: 0.9em;
+  --size-blog-main-text: 1em;
+  --font-family-text: "Inconsolata", monospace;
+  --spacing-blog-name-text: 0.3em;
+  --spacing-blog-info-text: 0.2em;
+  --spacing-blog-main-text: 0.1em;
+  --size-right-block-name-text: 1.4em;
+  --size-right-block-info-text: 0.5em;
+  --size-right-block-main-text: 0.8em;
+  --font-right-block-main-text: "Inconsolata", monospace;
+  --spacing-right-block-name-text: 0.1em;
+  --spacing-right-block-info-text: 0.1em;
+  --spacing-right-block-main-text: 0.1em;
 }
 </style>
