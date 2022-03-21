1da3f1d957e28e22d6596f5795a2b808574e3854
Vladyslav Levchuk <vladyslav.levchuk@wundermanthompson.com>
2022-03-21 10:53:41 +0100

    Components built in page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Blockimageleft from "./components/Block-image_left.vue";
+import Blockimageright from "./components/Block-image_right.vue";
+import Blockcarousel from "./components/Block-carousel.vue";
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Awesome project" />
+  <div class="wrapper">
+      <header class="header">
+        <div class="header_inner"></div>
+      </header>
+      <div class="wrapper_inner">
+        <div class="content_inner">
+  <Blockimageleft />
+  <Blockimageright />
+  <Blockcarousel />
+  </div>
+  </div>
+  </div>
 </template>
 
 <style>
@@ -17,5 +31,63 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+:root {
+  --main-bg-color: #f1f1f1;
+  --main-bgdiv-color: #ffffff;
+  --text-color-darkness: #333333;
+  --text-color-lightness: #bebebe;
+  --text-color-light: #777777;
+  --selected-div-color: #e94e4e;
+  --arrow-div-color: #3b3b3b;
+  --dark-border-btn-color:#222;
+  --red-border-and-bg-btn-color:#ea4e4e;
+  --red-hover-btn-kolor:#d23333;
+  --size-blog-name-text: 2.2em;
+  --size-blog-info-text: 0.9em;
+  --size-blog-main-text: 1em;
+  --font-family-text: "Inconsolata", monospace;
+  --spacing-blog-name-text: 0.3em;
+  --spacing-blog-info-text: 0.2em;
+  --spacing-blog-main-text: 0.1em;
+  --size-right-block-name-text: 1.4em;
+  --size-right-block-info-text: 0.5em;
+  --size-right-block-main-text: 0.8em;
+  --font-right-block-main-text: "Inconsolata", monospace;
+  --spacing-right-block-name-text: 0.1em;
+  --spacing-right-block-info-text: 0.1em;
+  --spacing-right-block-main-text: 0.1em;
+}
+* {
+  box-sizing: border-box;
+}
+html,
+body {
+  padding: 0;
+  margin: 0;
+}
+body {
+  position: relative;
+  background-color: var(--main-bg-color);
+  font-family: var(--font-family-text);
+  line-height: 1.6;
+  font-size: 17px;
+  color: var(--text-color-darkness);
+}
+.header {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  width: 60px;
+  background-color: green;
+  z-index: 99998;
+  overflow: hidden;
+}
+.wrapper_inner {
+  margin: 3%;
+  position: relative;
+}
+.content_inner {
+  margin-left: 60px;
 }
 </style>
---
+++ src/components/Block-image_left.vue
@@ -0,0 +1,124 @@
+<script setup></script>
+
+<template>
+
+  <div class="block_left_image">
+    <div class="block_image">
+      <img src="../assets/space.jpg" alt="image" />
+    </div>
+    <div class="content_block">
+      <div class="content_left">
+        <h1>Who i am</h1>
+        <p class="description">Artist & Photographer</p>
+        <p class="text">
+          Hi, my name is Martin Vegas. I am an artist and photographer.
+          Sollicitudin diam vitae, amet lacus donec eu, donec vulputate duis
+          nullam nulla, suscipit nulla orci, ornare maecenas eget gravida. Curae
+          sollicitudin lobortis.
+        </p>
+        <div class="block_icon">
+          <ul>
+            <li>
+              <a title="Folow me on facebook" href="#">
+                <i class="fa fa-facebook"></i>
+              </a>
+            </li>
+            <li>
+              <a title="Folow me on twitter" href="#">
+                <i class="fa fa-twitter"></i>
+              </a>
+            </li>
+            <li>
+              <a title="Folow me on instagram" href="#">
+                <i class="fa fa-instagram"></i>
+              </a>
+            </li>
+            <li>
+              <a title="Email me" href="#">
+                <i class="fa fa-envelope-o"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.block_left_image{
+  background-color: var(--main-bgdiv-color);
+  height: 560px;
+  width: 100%;
+  display: flex;
+}
+.block_image {
+  height: 100%;
+  width: 50%;
+  z-index: 1;
+}
+img {
+  width: 100%;
+  height: 100%;
+}
+.content_block {
+  width: 50%;
+  height: 100%;
+  display: flex;
+}
+.content_left {
+  background-color: var(--main-bgdiv-color);
+  margin: auto 0 auto -15%;
+  padding: 10% 30% 10% 10%;
+  text-align: left;
+  border-radius: 4px;
+  z-index: 10;
+}
+.block_icon {
+  margin-top: 30px !important;
+}
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+ul > li:first-child {
+  margin-left: 0px;
+}
+li {
+  display: inline-block;
+  margin: 5px;
+}
+a {
+  font-size: 18px;
+  color:var( --dark-border-btn-color);
+}
+i {
+  font-family: "Font Awesome";
+  font-weight: 400;
+  display: inline-block;
+  line-height: 1;
+}
+h1 {
+  text-transform: uppercase;
+  color: var(--text-color-darkness);
+  margin: 0 0 15px 0;
+  font-size: 26px;
+  line-height: 1.1;
+}
+.description {
+  margin-bottom: 30px;
+  font-size: 17px;
+  font-style: italic;
+  color: var(--text-color-lightness);
+}
+p {
+  margin: 0 0 1em;
+  line-height: 26px;
+}
+
+.text {
+  margin-bottom: 20px;
+  font-size: 15.5px;
+}
+</style>
